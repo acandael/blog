09e47b3573ab989c09057b622c641b64e73fec6f
acandael <info@anthonycandaele.com>
2020-06-16 14:51:15 +0200

    issue with reducer fetchPosts

---
+++ src/components/PostList.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions';
+
+class PostList extends React.Component {
+  componentDidMount() {
+    this.props.fetchPosts();
+  }
+  render() {
+    return <div>PostList</div>;
+  }
+}
+
+const mapStateToProps = (state) => {
+  return { posts: state.posts };
+};
+
+export default connect(mapStateToProps, fetchPosts)(PostList);
